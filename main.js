ddbcf9c9c6f81eba4cdef3386cc397df2435b0e4
Deividi_Lucas <113848434+Deividi-Lucas@users.noreply.github.com>
2023-01-03 19:37:26 -0400

    Primeiro upload site

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-let mnt = document.createElement('h1')
-mnt.innerHTML = `EM MANUTENÇÃO!!!`
-mnt.style.color = 'white'
-document.body.append(mnt)
 var footer = window.document.getElementsByTagName('footer')
 let main = window.document.getElementById('main')
 let nome = window.document.getElementsByTagName('h1')
@@ -9,10 +5,8 @@ let local = document.getElementsByTagName('h3')
 let personal = document.getElementsByClassName('about-me')
 let text = document.getElementById('text')
 let contact = document.getElementById('contact')
-let project = document.getElementById('project')
 
 main.style.cssText = 'height: 100vh; display:flexbox; text-align:center;'
-project.style.cssText = 'height: 100vh; padding:20px;'
 
 function color(){
     document.body.style.background = '#282828'
@@ -23,12 +17,7 @@ function color(){
         cont ++
     }
     cont = 0
-    while (cont < project.getElementsByTagName('p').length){
-        project.getElementsByTagName('p').item(cont).style.color = 'white'
-        project.getElementsByTagName('p').item(cont).style.padding = '50px'
-        cont ++
-    }
-    cont = 0
+   
     var img = document.getElementById('logo')
     footer.item(0).style.cssText = 'background: #d12323; color:white; font-size: large;'
     nome.item(0).style.color = 'white' // Cor
