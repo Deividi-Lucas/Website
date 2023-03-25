ec88251bb003d4756a17915d20269ac1d6f50207
unknown <deividibaby6@gmail.com>
2023-03-25 18:59:24 -0400

    chore [new website in react]

--- src/pages/main/App.js
+++ src/pages/main/App.js
@@ -1,21 +1,10 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <img src="images/deividi_page.jpg" alt="picture" />
     </div>
   )
 }
