7ecf6bbe8e7bacb85a4d23f79552a646581e3da2
unknown <deividibaby6@gmail.com>
2023-03-26 15:30:00 -0400

    feat [append my name and my from city]

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,7 +6,9 @@ export default function Main() {
   return (
     <div className="main_container">
       <Header />
-      <img src={perfil_photo} alt="Mymainphoto" />
+      <img src={perfil_photo} alt="Mymainphoto" className="perfil-main" />
+      <h1>Deividi Lucas</h1>
+      <h4>From Cuiaba-MT</h4>
     </div>
   )
 }
