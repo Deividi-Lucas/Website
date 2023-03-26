673d931d7ffa89ce949c9b3015fcf5e1cfc9bb49
unknown <deividibaby6@gmail.com>
2023-03-25 20:11:17 -0400

    chore [NavBar in production]

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './pages/global/style.scss'
+import Header from './components/header'
+import Main from './pages/home'
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import './style.scss'
+import dev from '../../images/Dev.png'
+
+export default function Header() {
+  return (
+    <header>
+      <img src={dev} />
+      <li>
+        <a href="#">Sobre mim</a>
+        <a href="#">Contato</a>
+        <a href="#">Projetos</a>
+      </li>
+    </header>
+  )
+}
