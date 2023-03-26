df3235ff12e0db553caaebd1ff679c049c1a4cc0
unknown <deividibaby6@gmail.com>
2023-03-25 20:58:59 -0400

    style [add img main]

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import './style.scss'
 import Header from '../../components/header'
+import perfil_photo from '../../images/deividi_page.jpg'
 
 export default function Main() {
   return (
-    <>
+    <div className="main_container">
       <Header />
-    </>
+      <img src={perfil_photo} alt="Mymainphoto" />
+    </div>
   )
 }
