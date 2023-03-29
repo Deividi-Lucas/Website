71d88f78256aae3e7c0515c0d297d56cef250f82
unknown <deividibaby6@gmail.com>
2023-03-29 09:32:30 -0400

    feat [added project converte-cash]

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,6 +1,7 @@
 import './style.scss'
 import Header from '../../components/header'
 import personal_site from '../../images/Website-personal.jpg'
+import project_converter from '../../images/Converte-Cash.png'
 
 export default function Project() {
   return (
@@ -15,6 +16,14 @@ export default function Project() {
               className="website_project"
             />
             <span>Website-pessoal</span>
+          </div>
+          <div className="projectConverter">
+            <img
+              src={project_converter}
+              alt="projeto_converta_cash"
+              className="ConverterCash"
+            />
+            <span>Convertor de dinheiro</span>
           </div>
         </div>
       </main>
