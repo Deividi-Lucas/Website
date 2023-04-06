4fcedabc38240630803815d0c5cb86eb68d85f02
unknown <deividibaby6@gmail.com>
2023-04-06 15:45:24 -0400

    chore [ new project added and project personal comment ]

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,8 +1,9 @@
 import './style.scss'
 import Header from '../../components/header'
 import personal_site from '../../images/Website-personal.jpg'
-import project_converter from '../../images/Converte-Cash.png'
+import project_converter from '../../images/Converter-Cash.png'
 import timer_project from '../../images/Timer.png'
+import searchcep from '../../images/SearchCep.png'
 
 export default function Project() {
   return (
@@ -10,14 +11,15 @@ export default function Project() {
       <Header />
       <main>
         <div className="site">
-          <div className="projectSite">
+          {/* <div className="projectSite">
             <img
               src={personal_site}
               alt="personal-site"
               className="website_project"
             />
             <span>Website-pessoal</span>
-          </div>
+          </div> */}
+
           <div className="projectConverter">
             <img
               src={project_converter}
@@ -26,6 +28,16 @@ export default function Project() {
             />
             <span>Convertor de dinheiro</span>
           </div>
+
+          <div className="SearchCepProject">
+            <img
+              src={searchcep}
+              alt="Projeto_SearchCep"
+              className="SearchCep"
+            />
+            <span>Procurador de Cep</span>
+          </div>
+
           <div className="TimerProject">
             <img src={timer_project} alt="Projeto_timer" className="timer" />
             <span>Temporizador</span>
