18e35f2778604aa6cec8dea264dabd947a302236
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-15 20:18:57 -0400

    Feat: new props created

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -7,17 +7,19 @@ import searchcep from '../../images/SearchCep.png'
 import machine from '../../images/machine.png'
 import poke from '../../images/PokeGadget.png'
 import Post from '../../components/card'
+import { color } from '../../utils/color'
+
 
 export default function Project() {
   return (
     <div className="Project">
       <Header />
       <main>
-      <Post img={project_converter} alt='Converter Cash' span='Converter Cash' link='https://converter-cash.vercel.app/'/>
-      <Post img={timer_project} alt='Timer_project' span='Temporizador' />
-      <Post img={searchcep} alt='SearchCep' span='Procurador de Cep' link='https://search-cep-gamma.vercel.app/'/>
+      <Post img={project_converter} alt='Converter Cash' span='Converter Cash' link='https://converter-cash.vercel.app/' background={color.green}/>
+      <Post img={timer_project} alt='Timer_project' span='Temporizador' background={color.red}/>
+      <Post img={searchcep} alt='SearchCep' span='Procurador de Cep' link='https://search-cep-gamma.vercel.app/'background={color.yellow}/>
       <Post img={machine} alt='MachineSpace' span='Machine Space' link='https://machine-space.vercel.app/'/>
-      <Post img={poke} alt='PokeGadget' span='PokeGadgets' link='https://poke-gadgets.vercel.app/'/>
+      <Post img={poke} alt='PokeGadget' span='PokeGadgets' link='https://poke-gadgets.vercel.app/'background={color.orange}k/>
       </main>
     </div>
   )
---
+++ src/utils/color.js
@@ -0,0 +1,8 @@
+export const color = {
+    'white':'#fffadf',
+    'green':'#008909',
+    "red":"#a51b0b",
+    'yellow':"#cece31",
+    'orange':"#e69500"
+
+}
