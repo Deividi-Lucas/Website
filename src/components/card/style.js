94b843813051758884d56b962888c3281ade4c51
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-22 20:53:37 -0400

    Chore: new color added

--- src/components/card/style.js
+++ src/components/card/style.js
@@ -1,35 +1,31 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Card = styled.div`
-    height: min-content;
+  height: min-content;
 
-    display: flex;
-    flex-direction: column;
-    padding: 2.2rem ;
-    flex-wrap: wrap;
-    background: rgba(0,174,255,0.7);
-    margin: 1rem;
-    border-radius: 4rem 1rem;
-    
-    
-    a{
-        gap: 1rem;
-        
+  display: flex;
+  flex-direction: column;
+  padding: 2.2rem;
+  flex-wrap: wrap;
+  background: rgba(0, 0, 0);
+  margin: 1rem;
+  border-radius: 4rem 1rem;
+
+  a {
+    gap: 1rem;
+  }
+
+  img {
+    height: 6rem;
+    transition: 100ms;
+
+    &:hover {
+      transform: scale(120%);
     }
+  }
 
-    img{
-        height: 6rem;
-        transition: 100ms;
-        
-        &:hover{
-
-            transform: scale(120%);
-        }
-    }
-
-    span{
-        color: white;
-        text-align: center;
-    }
-
+  span {
+    color: white;
+    text-align: center;
+  }
 `
--- src/utils/color.js
+++ src/utils/color.js
@@ -1,8 +1,9 @@
 export const color = {
-    'white':'#fffadf',
-    'green':'#008909',
-    "red":"#a51b0b",
-    'yellow':"#cece31",
-    'orange':"#e69500"
-
+  white: '#fffadf',
+  green: '#008909',
+  red: '#a51b0b',
+  yellow: '#cece31',
+  orange: '#e69500',
+  black: '#191919',
+  gray: '#666666'
 }
