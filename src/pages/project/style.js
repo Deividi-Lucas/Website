d77e4ad6ba0c5f60473d950dce6036a680c7f846
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-22 20:50:52 -0400

    Feat: Convert Sass in js-in-css

---
+++ src/pages/project/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+`
