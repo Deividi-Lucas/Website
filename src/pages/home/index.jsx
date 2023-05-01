5c369ed970647878a54ced1f6425e9fbd3547f5b
Deividi-Lucas <deividibaby6@gmail.com>
2023-05-01 18:07:45 -0400

    Feat: new area for Contact

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,36 +1,58 @@
 import './style.scss'
-import Header from '../../components/header'
+import Header from '../../components/Header'
 import perfil_photo from '../../images/deividi_page.jpg'
-import { About, Main } from './style'
+import linkedin from '../../images/linkedin.png'
+import github from '../../images/github.png'
+import { About, Contact, Main } from './style'
+import ContactCard from '../../components/CardContact'
 export default function Home() {
   return (
-    <div className="main_container">
-      <Header />
-      <Main>
-        <div>
-        <img src={perfil_photo} alt="Mymainphoto" className="perfil-main" />
-        <h1>Deividi Lucas</h1>
-        <h4>From Cuiaba-MT</h4>
-        </div>
-        <About>
-          <h2>Sobre mim</h2>
-          <p>
-            Olá, sou Deividi Lucas, sou brasileiro, Tenho atualmente 19 anos.
-            Atualmente trabalho no Exército brasileiro como Auxiliar de Ti,
-            mexendo com a infraestrutura de rede. Por tanto tive interesse em
-            mexer com programação a partir de 2021, por causa de um evento escolar
-            do ensino médio, neste evento foi apresentando uma faculdade na área
-            de tecnologia de informação e apresentado a grade curricular. Na qual
-            estava descrito a área de programação e uma linguagem 'python', desde
-            já venho estudando sobre programação desde de dezembro de 2021. A
-            primeira linguagem na qual eu tive contado foi python, porém não
-            mantive um estudo por muito tempo. Nesta caminhada de estudo resolvi
-            adquirir conhecimento no Front-end pelo qual um colega de trabalho
-            indicou. Agora mantenho estudando sobre Front-end desenvolvendo em
-            HTML | CSS | JAVASCRIPT
-          </p>
-        </About>
-      </Main>
-    </div>
+    <>
+      <div className="main_container">
+        <Header />
+        <Main>
+          <div>
+            <img src={perfil_photo} alt="Mymainphoto" className="perfil-main" />
+            <h1>Deividi Lucas</h1>
+            <h4>From Cuiaba-MT</h4>
+          </div>
+          <About>
+            <h2>Sobre mim</h2>
+            <p>
+              Olá, sou Deividi Lucas, sou brasileiro, Tenho atualmente 19 anos.
+              Atualmente trabalho no Exército brasileiro como Auxiliar de Ti,
+              mexendo com a infraestrutura de rede. Por tanto tive interesse em
+              mexer com programação a partir de 2021, por causa de um evento
+              escolar do ensino médio, neste evento foi apresentando uma
+              faculdade na área de tecnologia de informação e apresentado a
+              grade curricular. Na qual estava descrito a área de programação e
+              uma linguagem 'python', desde já venho estudando sobre programação
+              desde de dezembro de 2021. A primeira linguagem na qual eu tive
+              contado foi python, porém não mantive um estudo por muito tempo.
+              Nesta caminhada de estudo resolvi adquirir conhecimento no
+              Front-end pelo qual um colega de trabalho indicou. Agora mantenho
+              estudando sobre Front-end desenvolvendo em HTML | CSS | JAVASCRIPT
+            </p>
+          </About>
+        </Main>
+        <Contact>
+          <h1>Contact</h1>
+
+          <ContactCard
+            text="linkedin"
+            img={linkedin}
+            alt="Linkedin_photo"
+            link="https://www.linkedin.com/in/deividi-lucas-a60612254/"
+          />
+
+          <ContactCard
+            text="github"
+            img={github}
+            alt="github_photo"
+            link="https://github.com/Deividi-Lucas"
+          />
+        </Contact>
+      </div>
+    </>
   )
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,27 +1,40 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const About = styled.div`
-display: flex;
-flex-direction: column;
-margin: 2rem;
-background-color: rgb(0,0,0,0.8);
-padding: 1rem ;
-border-radius: 1rem 0;
-width: 40%;
-h2{
+  display: flex;
+  flex-direction: column;
+  margin: 2rem;
+  background-color: rgb(0, 0, 0, 0.8);
+  padding: 1rem;
+  border-radius: 1rem 0;
+
+  h2 {
     text-align: start;
-    
-}
-p{
+  }
+  p {
     text-align: justify;
-}
-
-
+  }
 `
 
 export const Main = styled.div`
-display: flex;
-margin: 2rem;
-justify-content: center;
-align-items: center;
+  display: flex;
+  margin: 2rem;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export const Contact = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5rem;
+  @media (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+  }
 `
