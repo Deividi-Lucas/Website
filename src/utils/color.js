8483d2a45cb81c66c4b548f95a7fbbdee9351670
Deividi-Lucas <deividibaby6@gmail.com>
2023-06-19 20:09:17 -0400

    Style: New palet color

--- src/utils/color.js
+++ src/utils/color.js
@@ -6,5 +6,9 @@ export const color = {
   orange: '#e69500',
   black: '#191919',
   gray: '#666666',
-  roxo: '#551985'
+  roxo: '#551985  ',
+  primary: '#0d9ddb',
+  secondary: '#4d4e5f',
+  tertiary: '#fcf6f5',
+  quarternary: '#e8eaf1'
 }
