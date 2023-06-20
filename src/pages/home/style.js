f1eb181d033e393d2209cce1bb29b7645e221a19
Deividi-Lucas <deividibaby6@gmail.com>
2023-06-19 20:10:11 -0400

    Refactor: Delete row no util

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -4,9 +4,9 @@ export const About = styled.div`
   display: flex;
   flex-direction: column;
   margin: 2rem;
-  background-color: rgb(0, 0, 0, 0.8);
   padding: 1rem;
   border-radius: 1rem 0;
+  gap: 0.5rem;
 
   h2 {
     text-align: start;
@@ -21,20 +21,5 @@ export const Main = styled.div`
   margin: 2rem;
   justify-content: center;
   align-items: center;
-  @media (max-width: 600px) {
-    display: flex;
-    flex-direction: column;
-  }
-`
-
-export const Contact = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 5rem;
-  @media (max-width: 600px) {
-    display: flex;
-    flex-direction: column;
-  }
+  flex-direction: column;
 `
