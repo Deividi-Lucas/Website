e5260ad7dc1149c7cccd0fefb4f5c0f0317bba1d
Deividi-Lucas <deividibaby6@gmail.com>
2023-10-01 11:31:02 -0400

    Chore: Excluindo projetos não utilizado

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,14 +1,8 @@
 import Header from '../../components/Header'
 // import personal_site from '../../images/Website-personal.jpg'
 import project_converter from '../../images/Converter-Cash.png'
-import timer_project from '../../images/Timer.png'
 import searchcep from '../../images/SearchCep.png'
-import machine from '../../images/machine.png'
-import poke from '../../images/PokeGadget.png'
 import Post from '../../components/Card'
-import WhatToDo from '../../images/WhatToDo.png'
-import GitPersonal from '../../images/GitPersonal.png'
-import CalcTimer from '../../images/CalcTimer.png'
 import { color } from '../../utils/color'
 import { Main } from './style'
 
@@ -24,52 +18,13 @@ export default function Project() {
           link="https://converter-cash.vercel.app/"
           background={color.green}
         />
-        <Post
-          img={timer_project}
-          alt="Timer_project"
-          span="Temporizador"
-          background={color.red}
-        />
+
         <Post
           img={searchcep}
           alt="SearchCep"
           span="Procurador de Cep"
           link="https://search-cep-gamma.vercel.app/"
           background={color.yellow}
-        />
-        <Post
-          img={machine}
-          alt="MachineSpace"
-          span="Machine Space"
-          link="https://machine-space.vercel.app/"
-          background={color.black}
-        />
-        <Post
-          img={poke}
-          alt="PokeGadget"
-          span="PokeGadgets"
-          link="https://poke-gadgets.vercel.app/"
-          background={color.orange}
-        />
-        <Post
-          img={WhatToDo}
-          alt="WhatToDo"
-          span="What to do"
-          link="https://what-to-do-eight.vercel.app/"
-          background={color.gray}
-        />
-        <Post
-          img={GitPersonal}
-          alt="GitPersonal"
-          span="GitPersonal"
-          background={color.roxo}
-          link="https://git-personal.vercel.app/"
-        />
-        <Post
-          img={CalcTimer}
-          span="CalcTimer"
-          alt="CalcTimer"
-          link="https://calc-timer.vercel.app"
         />
       </div>
     </Main>
